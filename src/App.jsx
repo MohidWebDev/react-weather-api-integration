f72c0d4eaf2084f305c0a03e3c2d9ec7e4f0ac7a
Mohid Yaseen <mmohid069@gmail.com>
2026-04-18 05:17:29 -0700

    Modified the import statements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import AreaCalculator from "./components/AreaCalculator";
-import BMICalculator from "./components/BMICalculator";
 import ECommerce from "./components/ECommerce";
 import Header from "./components/Header";
 
